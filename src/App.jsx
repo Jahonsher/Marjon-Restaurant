ebc301bc90b2d1fc38f8d42328f7630a0ec18c68
Jahonsher <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>
2024-08-24 12:26:50 +0500

    salom

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import'./index.css'
 const App = () => {
   return (
     <>
-      <div className='general '>
+      {/* <div className='general '>
         <img className='w-[95%] h-[200px] pt-2 m-[auto] rounded-[20px] ' src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcS4Aoac14mXleRqONrs6b8BjoihLQ1uBsQK_Oo5cb8eFXGsxelp" alt="rasm" />
         <div className="box"></div>
-      </div>
+      </div> */}
     </>
   )
 }
